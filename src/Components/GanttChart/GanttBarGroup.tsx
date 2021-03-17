b1dba20fa61df56a3e1b237262f559f85b93cbf4
Carlos Aguinaga <andres.aguinaga@gmail.com>
2021-03-16 23:19:51 -0500

    Update GanttBarGroup.tsx

--- src/Components/GanttChart/GanttBarGroup.tsx
+++ src/Components/GanttChart/GanttBarGroup.tsx
@@ -17,24 +17,24 @@ type Props ={
 
 const GanttBarGroup = (props: Props) => {
 
-        const has_groups = props.items.filter((item: any) => item.type === 'group').length ? true : false
+    const has_groups = props.items.filter((item: any) => item.type === 'group').length ? true : false
 
-        const durationAndProgress = UI_helper.getGroupDuration(props.items)
-        const progress = UI_helper.getGroupProgress(props.items)
-        const bars = UI_helper.getGroupBars(props.items)
+    const durationAndProgress = UI_helper.getGroupDuration(props.items)
+    const progress = UI_helper.getGroupProgress(props.items)
+    const bars = UI_helper.getGroupBars(props.items)
 
-        if (props.collapseStatus === 'collapsed') {
+    if (props.collapseStatus === 'collapsed') {
 
-            bars.map((bar: any) => {
+        bars.map((bar: any) => {
 
-                bar.x = UI_helper.getDayPosition(props.ganttStart, bar.startsAt) * props.dayWidth
-                bar.w = UI_helper.getDaysLength(bar.startsAt, bar.endsAt) * props.dayWidth
-            })
-        }
+            bar.x = UI_helper.getDayPosition(props.ganttStart, bar.startsAt) * props.dayWidth
+            bar.w = UI_helper.getDaysLength(bar.startsAt, bar.endsAt) * props.dayWidth
+        })
+    }
 
-        const x = UI_helper.getDayPosition(props.ganttStart, durationAndProgress.startsAt) * props.dayWidth
+    const x = UI_helper.getDayPosition(props.ganttStart, durationAndProgress.startsAt) * props.dayWidth
 
-        const w = UI_helper.getDaysLength(durationAndProgress.startsAt, durationAndProgress.endsAt) * props.dayWidth
+    const w = UI_helper.getDaysLength(durationAndProgress.startsAt, durationAndProgress.endsAt) * props.dayWidth
 
     return (
         <>
